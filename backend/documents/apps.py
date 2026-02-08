9f5ce760a2831ccd53cf873b906f709f7331aedf
Akashmdshetty <aakashshetty112233@gmail.com>
2026-02-08 14:46:05 +0530

    Update apps.py

--- backend/documents/apps.py
+++ backend/documents/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class DocumentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.documents'
+    name = 'documents'
