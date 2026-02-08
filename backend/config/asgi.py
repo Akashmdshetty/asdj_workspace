bc8e7f2643813250f507801d7da1e7ec8a4e7490
Akashmdshetty <aakashshetty112233@gmail.com>
2026-02-08 15:31:13 +0530

    Update asgi.py

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,19 +1,19 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import backend.collaboration.routing
+import collaboration.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            backend.collaboration.routing.websocket_urlpatterns
+            collaboration.routing.websocket_urlpatterns
         )
     ),
 })
